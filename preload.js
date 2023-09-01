bd47882436d9511308eae3f9135537bb72ece820
Pratik <pratik2201.pp@gmail.com>
2023-09-02 02:21:18 +0530

    REMOVED UNNESASSORY FILES

--- preload.js
+++
@@ -1,9 +0,0 @@
-/*const tbl_ledger = require('@ucbuilder:/Database/tbl_ledger');
-//const {contextBridge} = require('electron');
-const getNames = () =>{
-    return tbl_ledger.getNames();
-};*/
-/*
-contextBridge.exposeInMainWorld("api",{
-    getNames:getNames
-});*/
