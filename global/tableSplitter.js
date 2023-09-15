12be4324451e2142205755a4ef83f9b8a0592a4a
ap <pratik2201.pp@gmail.com>
2023-09-15 08:50:42 +0530

    Update tableSplitter.js

--- global/tableSplitter.js
+++ global/tableSplitter.js
@@ -1,10 +1,21 @@
+const tableParam = {
+    /** @type {HTMLElement} */
+    table : undefined, 
+    /** @type {string} */
+    trTagName : undefined,  
+    /** @type {string} */
+    tdTagName : undefined
+};
 class tableSplitter{
   constructor(){
     
   }
   /** @type {HTMLElement} */
-  tableHT = undefined;
-  init(){
+  table = undefined;
+  /** @param {tableParam} param0 */
+  init(param0){
+    let options = 
+    this.table = param0.table;
     
   }
 }
