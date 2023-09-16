58f24c3a9dff07968cf424a6d43c3ace49953b1b
Pratik <pratik2201.pp@gmail.com>
2023-09-17 01:23:03 +0530

    .

--- global/gridResizer.js
+++ global/gridResizer.js
@@ -3,8 +3,27 @@ const measurementRow = {
     size: undefined,
     data: {}
 };
-class gridResizer {    
-    constructor() {}
+class gridResizer {
+    constructor() {
+    }
+    /** @type {HTMLElement}  */ 
+    grid = undefined;
+    templeteOf = 'grid-template-columns';
+    /** @type {measurementRow[]}  */
+    measurement = [];
 
+    get measureText() {
+        return this.measurement.length <= 1 ? 'auto'
+            : this.measurement
+                .map(s => s.size)
+                .slice(0, -1)
+                .join('px ') + 'px auto';
+    }
+    refreshView() {
+        this.grid.style[this.templeteOf] = this.measureText;
+    }
+    get hasDefinedStyles() {
+        return this.grid.style[this.templeteOf] != "";
+    }
 }
-module.exports = { gridResizer,measurementRow }
+module.exports = { gridResizer, measurementRow }
