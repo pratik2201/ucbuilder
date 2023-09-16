07253b84496119040477e89a6ee5fa080c9b527a
Pratik <pratik2201.pp@gmail.com>
2023-09-17 00:48:38 +0530

    ..

---
+++ global/gridResizer.js
@@ -0,0 +1,10 @@
+const measurementRow = {
+    /** @type {number}  */
+    size: undefined,
+    data: {}
+};
+class gridResizer {    
+    constructor() {}
+
+}
+module.exports = { gridResizer,measurementRow }
