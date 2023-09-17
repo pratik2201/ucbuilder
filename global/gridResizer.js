52fe714e5469246c2be688f7af7d59c5a3e58a76
Pratik <pratik2201.pp@gmail.com>
2023-09-17 13:53:32 +0530

    .

--- global/gridResizer.js
+++ global/gridResizer.js
@@ -4,17 +4,62 @@ const measurementRow = {
     data: {}
 };
 class gridResizer {
+    nameList = {
+        offsetSize: 'offsetWidth',
+        splitterText: 'splitter-width',
+        templeteOf: 'grid-template-columns',
+        point: 'x',
+        size: 'width',
+        dir: 'left',
+        pagePoint: 'pageX',
+        /** @param {spliterType} type */
+        setByType: (type) => {
+            let _this = this.nameList;
+            switch (type) {
+                case 'grid-template-columns':
+                    _this.offsetSize = 'offsetWidth';
+                    _this.splitterText = 'splitter-width';
+                    _this.templeteOf = type;
+                    _this.size = 'width';
+                    _this.point = 'x';
+                    _this.dir = "left";
+                    _this.pagePoint = 'pageX';
+                    break;
+                case 'grid-template-rows':
+                    _this.offsetSize = 'offsetHeight';
+                    _this.splitterText = 'splitter-height';
+                    _this.templeteOf = type;
+                    _this.size = 'height',
+                    _this.point = 'y';
+                    _this.dir = "top";
+                    _this.pagePoint = 'pageY';
+                    break;
+            }
+        }
+    }
     constructor() {
     }
-    /** @type {HTMLElement}  */ 
-    grid = undefined;
+    /** @type {HTMLElement}  */
+    _grid = undefined;
+    get grid() {
+        return this._grid;
+    }
+    set grid(value) {
+        this._grid = value;
+        if(this.container==undefined) this.container = this._grid;
+    }
+    /** @type {HTMLElement}  */
+    container = undefined;
+
     templeteOf = 'grid-template-columns';
     /** @type {measurementRow[]}  */
     measurement = [];
-
+    /** @type {"fill"|"unfill"}*/
+    fillMode = "unfill";
     /** @type {HTMLElement}  */
     static rectHT = `<resizer role="drawSelection"></resizer>`.$();
-    
+    /** @type {HTMLElement}  */
+    static resizerHT = `<resizer role="left"></resizer>`.$();
     get measureText() {
         return this.measurement.length <= 1 ? 'auto'
             : this.measurement
@@ -23,10 +68,11 @@ class gridResizer {
                 .join('px ') + 'px auto';
     }
     refreshView() {
-        this.grid.style[this.templeteOf] = this.measureText;
+        //this, this.grid.contains
+        this.grid.style[this.nameList.templeteOf] = this.measureText;
     }
     get hasDefinedStyles() {
-        return this.grid.style[this.templeteOf] != "";
+        return this.grid.style[this.nameList.templeteOf] != "";
     }
 }
 module.exports = { gridResizer, measurementRow }
