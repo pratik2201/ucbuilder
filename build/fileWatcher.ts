2051706607b8a1422ccb4962eddf14245ea0a5b3
pratik2201 <pratik2201.pp@gmail.com>
2024-10-30 22:48:44 +0530

    g

--- build/fileWatcher.ts
+++ build/fileWatcher.ts
@@ -1,36 +1,35 @@
-import fs  from "fs";
+import fs from "fs";
 import { builder } from "./builder";
 import { codeFileInfo } from "./codeFileInfo";
 
-export class fileWatcher{
+export class fileWatcher {
     constructor() { }
     main: builder;
     init(main: builder) {
         this.main = main;
-        
+
     }
-     startWatch(fPath:string) {        
-         this.watcher = fs.watch(fPath, { recursive: true }, this.watch_Listner)
+    startWatch(fPath: string) {
+        this.watcher = fs.watch(fPath, { recursive: true }, this.watch_Listner)
     }
-     stopWatch(fPath:string) {        
+    stopWatch(fPath: string) {
         this.watcher.close();
+
     }
-     watcher: fs.FSWatcher = undefined;
+    watcher: fs.FSWatcher = undefined;
 
     watch_Listner = (evt: fs.WatchEventType, filepath: string) => {
-        switch (evt) {
-            case "change":
-                if (filepath.endsWithI(codeFileInfo.___DESIGNER_EXT) || filepath.endsWithI(codeFileInfo.___DESIGNER_EXT)) {
+        filepath = filepath.toLowerCase();
+        if (filepath.endsWith(codeFileInfo.___DESIGNER_EXT) || filepath.endsWith(codeFileInfo.___DESIGNER_SRC_EXT)) {
+            switch (evt) {
+                case "change":
                     console.log(`CHANGED :- ${filepath}`);
-                    
-                }        
-                break;
-            case "rename":
-                if (filepath.endsWithI(codeFileInfo.___DESIGNER_EXT) || filepath.endsWithI(codeFileInfo.___DESIGNER_EXT)) {
+
+                    break;
+                case "rename":
                     console.log(`RENAMED :- ${filepath}`);
-                      
-                }   
-                break;
+                    break;
+            }
         }
     };
 }
